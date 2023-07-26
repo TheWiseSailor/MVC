bbed0f40b92920fb63db549d5d23196a0b988b68
TheWiseSailor <rudasilljay@gmail.com>
2023-07-26 13:10:37 -0400

    debugged server/index.js

--- models/index.js
+++ models/index.js
@@ -4,5 +4,8 @@ const Post = require("./Post");
 
 User.hasMany(Post, { as: "posts" });
 Post.belongsTo(User, { as: "user" });
+Post.hasMany(Comments, { as: "comments" });
+Comments.belongsTo(User, { as: "user" });
+Comments.belongsTo(Post, { as: "post" });
 
 module.exports = { User, Comments, Post };
--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-//import path
 const path = require("path");
-//import express
 const express = require("express");
-//import express handlebars
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const exphbs = require("express-handlebars");
-//import the routes ./controllers
 const routes = require("./controllers");
-//import sequelize for connection.js
 const sequelize = require("./config/connection");
-//handlebar
-// set the views directory to "views"
-app.set("views", path.join(__dirname, "views"));
 
-//create an express app instance
 const app = express();
-//initializes handlebars for HTML
-const hbs = exphbs.create({ helpers });
-//super super secret stuff lol
+const PORT = process.env.PORT || 3001;
+
 const sess = {
   secret: "Secret Secret Super",
   cookie: {},
@@ -28,21 +20,17 @@ const sess = {
 };
 
 app.use(session(sess));
-//You know what this does
-const PORT = process.env.PORT || 3001;
-
-//taken from the assignments in the bootcamp course just like connection.js
-// Inform Express.js which template engine we're using
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  //start listening for incoming requests
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () =>
+    console.log(`Now listening on http://localhost:${PORT}`)
+  );
 });
